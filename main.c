e4a82f146b056f8e8021a6f86e05b55fd75ba2d5
Harpal Singh Pannu <harpal06@gmail.com>
2019-04-30 15:21:01 -0400

    Hz

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <Windows.h>
+#include "crc.h"
+char *readable_fs(double size, char *buf)
+{
+    int i = 0;
+    const char *units[] = {"B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+    while (size > 1024){
+        size /= 1024;
+        i++;
+    }
+    sprintf(buf, "%.*f %s", i, size, units[i]);
+    return buf;
+}
+int main(void)
+{
+    char *filenames[5] =
+        {
+            "E:\\New folder\\1922.2017.720p.NF.WEBRip.x264.650MB-Pahe.in.mkv",
+            "Manzil.mp3"};
+    int sel = 0;
+    char buf[10];
+    crc_t crc;
+    clock_t start, end;
+    start = clock();
+    crc = crc_init();
+    FILE *file = NULL;
+
+    
+    
+
+    file = fopen(filenames[sel], "rb");
+    if (file == NULL)
+    {
+        printf("Error! Unable to read file\n");
+        return -1;
+    }
+    char buffer[1024];
+    DWORD bytesRead = 0;
+    HANDLE hFile = CreateFile(TEXT("Manzil.mp3"),
+                     GENERIC_READ,
+                     0,
+                     NULL,
+                     OPEN_EXISTING,
+                     FILE_ATTRIBUTE_NORMAL,
+                     NULL);
+    if(hFile == INVALID_HANDLE_VALUE){
+        printf("Error hFile");
+    }
+    double filesize = (double)GetFileSize(hFile,NULL);
+    double processed = 0;
+    printf("Name: %s \nSize: %s (%d Bytes)\n", filenames[sel],readable_fs(filesize, buf), (int)filesize);
+
+    
+
+    while (ReadFile(hFile, buffer, 1024, &bytesRead, NULL))
+    {
+        crc = crc_update(crc, buffer, bytesRead);
+    
+        processed += bytesRead;
+        double precent = (processed / filesize) * 100.0;
+       // printf("%d\r",(int)bytesRead);;
+       // printf("%0.2f%c \r", precent, '%');
+       // fflush(stdout);
+        if(bytesRead < 1024){
+            break;
+        }
+    }
+
+    crc = crc_finalize(crc);
+
+    printf("CRC32 Hexadecimal: 0x%lX\n", (unsigned long)crc);
+    printf("CRC32 Decimal: %lu\n", (unsigned long)crc);
+    end = clock();
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %f s\n", cpu_time_used);
+	
+    return 0;
+}
